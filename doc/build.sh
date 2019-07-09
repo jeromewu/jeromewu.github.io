1594fe4b5fe5300ca45dfac1606707e15338bbc6
Jerome Wu <jeromewus@gmail.com>
2019-07-09 15:07:29 +0800

    Update homepage

--- doc/build.sh
+++ doc/build.sh
@@ -8,7 +8,7 @@ gen_pdf() {
 
 build() {
   gen_pdf resume.tex
-  gen_pdf cv.tex
+  #gen_pdf cv.tex
   gen_pdf coverletter.tex
 }
 
--- src/App.js
+++ src/App.js
@@ -1,26 +1,37 @@
 import React from 'react';
-import logo from './logo.svg';
+import githubLogo from './images/github-logo.svg';
+import linkedinLogo from './images/linkedin-logo.svg';
+import resume from './images/resume.svg';
+import medium from './images/medium.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+export default () => (
+  <div className="app">
+    <div className="main">
+      <p className="name">Wen-Chieh Wu / Jerome Wu / 吳文傑</p>
+      <div className="imgs">
+        {
+          [
+            { href: 'https://github.com/jeromewu', src: githubLogo, alt: 'github-logo' },
+            { href: 'https://www.linkedin.com/in/wenchiehwu/', src: linkedinLogo, alt: 'linkedin-logo' },
+            { href: 'https://medium.com/@jeromewus', src: medium, alt: 'medium' },
+            { href: '/doc/resume.pdf', src: resume, alt: 'resume' },
+          ].map(({ href, src, alt  }) => (
+            <a key={alt} href={href} target="_blank" rel="noopener noreferrer">
+              <img className="img-button" src={src} alt={alt} />
+            </a>
+          ))
+        }
+      </div>
+      <p className="job-title">Technical Lead ‧ Software Engineer</p>
+      <p className="location">Taipei City, Taiwan</p>
     </div>
-  );
-}
-
-export default App;
+    <div className="footer">
+      Icon made by&nbsp;
+      <a href="https://www.flaticon.com/authors/simpleicon">simpleicon</a>,
+      <a href="https://www.freepik.com/">freepik</a>
+      &nbsp;from&nbsp;
+      <a href="http://www.flaticon.com/">www.flaticon.com</a>
+    </div>
+  </div>
+);
