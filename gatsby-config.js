344c55a5ac00008734a9323c57079b3723ce49b7
Jerome Wu <jeromewus@gmail.com>
2022-08-25 21:22:11 +0800

    feat: install plugins

--- gatsby-config.js
+++ gatsby-config.js
@@ -29,6 +29,10 @@ module.exports = {
       resolve: `gatsby-transformer-remark`,
       options: {
         plugins: [
+          `gatsby-remark-prismjs`,
+          `gatsby-remark-copy-linked-files`,
+          `gatsby-remark-smartypants`,
+          `gatsby-remark-graphviz`,
           {
             resolve: `gatsby-remark-images`,
             options: {
@@ -41,9 +45,6 @@ module.exports = {
               wrapperStyle: `margin-bottom: 1.0725rem`,
             },
           },
-          `gatsby-remark-prismjs`,
-          `gatsby-remark-copy-linked-files`,
-          `gatsby-remark-smartypants`,
         ],
       },
     },
